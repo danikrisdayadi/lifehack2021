9c279cbd173b6592043c35f64d6adee844948616
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-07-24 03:04:46 +0800

    Minor ui tweaks

--- client/src/components/navigationbar.jsx
+++ client/src/components/navigationbar.jsx
@@ -11,8 +11,8 @@ class NavigationBar extends Component {
           <img
             alt=""
             src={Logo}
-            width="30"
-            height="30"
+            width="40"
+            height="40"
             className="d-inline-block align-top"
           />
         </Navbar.Brand>
