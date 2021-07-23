2f7905c48a3053eeb31d17ab2ee0c16746f08d40
frisciliasultan <frisciliasultan@gmail.com>
2021-07-23 21:48:01 +0800

    Add sociallogin validation

---
+++ server/utils/validation/sociallogin.js
@@ -0,0 +1,32 @@
+import https from 'https';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const providers = {
+    google: {
+        url: 'https://oauth2.googleapis.com/',
+        qs: 'tokeninfo?id_token='
+    }
+};
+
+export const validateWithProvider = async (socialToken) => {
+    return new Promise((resolve, reject) => {
+        const url = providers[network].url;
+        const queryString = providers[network].qs + socialToken;
+
+        https
+            .get(url + queryString, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    resolve(JSON.parse(data));
+                });
+            })
+            .on('error', (err) => {
+                reject(err);
+            });
+    });
+};
