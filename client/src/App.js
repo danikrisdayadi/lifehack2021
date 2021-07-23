acfd6bc62cc0133014ecfbec155c6ea29661e146
Winy Febriny <e0426148@u.nus.edu>
2021-07-23 10:53:35 +0800

    install bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ExampleToast = ({ children }) => {
+    const [show, toggleShow] = useState(true);
+
+    return (
+        <>
+            {!show && (
+                <Button onClick={() => toggleShow(true)}>Show Toast</Button>
+            )}
+            <Toast show={show} onClose={() => toggleShow(false)}>
+                <Toast.Header>
+                    <strong className="mr-auto">React-Bootstrap</strong>
+                </Toast.Header>
+                <Toast.Body>{children}</Toast.Body>
+            </Toast>
+        </>
+    );
+};
+
+const App = () => (
+    <Container className="p-3">
+        <Alert variant="success">
+            <Alert.Heading>Hey, nice to see you</Alert.Heading>
+            <p>
+                Aww yeah, you successfully read this important alert message.
+                This example text is going to run a bit longer so that you can
+                see how spacing within an alert works with this kind of content.
+            </p>
+            <hr />
+            <p className="mb-0">
+                Whenever you need to, be sure to use margin utilities to keep
+                things nice and tidy.
+            </p>
+        </Alert>
+        <Jumbotron>
+            <h1 className="header">Welcome To React-Bootstrap</h1>
+            <ExampleToast>
+                We now have Toasts
+                <span role="img" aria-label="tada">
+                    🎉
+                </span>
+            </ExampleToast>
+        </Jumbotron>
+    </Container>
+);
 
 export default App;
