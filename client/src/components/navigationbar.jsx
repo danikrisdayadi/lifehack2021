5217b37b4ae88dc22e3c99dba44b993b0e8baeac
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-07-24 08:51:01 +0800

    Add login logout button

--- client/src/components/navigationbar.jsx
+++ client/src/components/navigationbar.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logoutUser } from '../redux/actions/AuthActions';
+import { Navbar, Nav, Container, NavDropdown, Button, Image } from 'react-bootstrap';
 import Logo from '../assets/logo-white.svg';
 
-const NavigationBar = (...props) => {
+const NavigationBar = ({...props}) => {
+    console.log(props)
     const logoutFunction = () => {
-
+        props.logoutUser();
+        props.history.push('/');
     }
+
+    let loginButton;
+    if (!props.auth.isAuthenticated) {
+        loginButton = (
+            <Nav.Link>
+                Login
+            </Nav.Link>
+        );
+    } else {
+        loginButton = (
+            <Nav.Link onClick={logoutFunction}>
+                Logout
+            </Nav.Link>
+        );
+    }
+
     return (
         <Navbar bg="info" variant="dark" style={{ marginBottom: '60px' }}>
             <Container>
@@ -32,13 +52,17 @@ const NavigationBar = (...props) => {
                     <Nav.Link as={Link} to="/profile">
                         My Profile
                     </Nav.Link>
-                    <Nav.Link onClick={logoutFunction}>
-                        Log Out
-                    </Nav.Link>
+                    {loginButton}
                 </Nav>
             </Container>
         </Navbar>
     );
 }
 
-export default NavigationBar;
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(
+    withRouter(NavigationBar)
+);;
