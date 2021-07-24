284a1b708e4eaf34ef25304a54100de5d87225e7
ZeGrandmaster <60135314+danikrisdayadi@users.noreply.github.com>
2021-07-24 09:21:46 +0800

    Add functionality

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import Coin from '../assets/coin.svg'
 import UserProfile from "../components/userprofile";
 import styled from "styled-components";
-
+import { AvatarPlaceholder } from "../utils/placeholder";
 const Style = styled.div`
     text-align: left;
 
 `
 const ProfilePage = () => {
-    const availableAvatars = ["dog", "bird", "rat"];
+    const [selectedAvatar, setselectedAvatar] = useState(AvatarPlaceholder);
+    const selectAvatar = (selectedName) => {
+        const newAvatar = {
+            name: selectedName,
+            picture: {location: '', key: ''},
+            price: 0,
+            unlocked: false,
+            _id: ''
+        }
+        console.log("Selectavatar")
+    }
+
+    const dogAvatar = {
+        name: 'dog',
+        picture: { location: 'dog.png', key: '' },
+        price: 100,
+        unlocked: true,
+        _id: ''
+    }
+    const birdAvatar = {
+        name: 'bird',
+        picture: { location: 'bird.png', key: '' },
+        price: 150,
+        unlocked: false,
+        _id: ''
+    }
+    const ratAvatar = {
+        name: 'rat',
+        picture: { location: 'rat.png', key: '' },
+        price: 250,
+        unlocked: false,
+        _id: ''
+    }
+    const availableAvatars = [dogAvatar, birdAvatar, ratAvatar];
     const avatars = availableAvatars.map((avatar) => {
         return (
             <Col >
                 <Row className="justify-content-center" style={{height: 100}}>
-                    <img src={`${process.env.PUBLIC_URL}/avatars/${avatar}.png`} alt="avatar" width="150"/>
+                    <img src={`${process.env.PUBLIC_URL}/avatars/${avatar.picture.location}`} alt="avatar" width="150"/>
                 </Row>
                 <Row className="justify-content-center">
-                    <Button style={{marginTop: 20}}>Select</Button>
+                    {avatar.unlocked ? 
+                        <Button style={{marginTop: 20}} onClick={selectAvatar(avatar)}>Select</Button> : 
+                        <Button style={{marginTop: 20}} onClick={selectAvatar(avatar)} variant="success">{avatar.price} <Image src={Coin} width="20px" style={{marginLeft: 5}}/></Button>}
+                    
                 </Row>
             </Col>
         )
